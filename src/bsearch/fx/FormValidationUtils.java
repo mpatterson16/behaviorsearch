ebc82b0a10d9c0e9b737e2979fd0400d36e0e933
Maegan Patterson <maeganpatterson16@augustana.edu>
2018-04-05 00:16:29 -0500

    Improved checking number fields and moved to its own class

---
+++ src/bsearch/fx/FormValidationUtils.java
@@ -0,0 +1,34 @@
+
+package bsearch.fx;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+public class FormValidationUtils {
+	// based on code by Evan Knowles
+	// https://stackoverflow.com/questions/7555564/what-is-the-recommended-way-to-make-a-numeric-textfield-in-javafx
+	public static void enforceNumericInput(TextField field, boolean allowDouble) {
+		field.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				if (allowDouble) {
+					if (!newValue.matches("[0-9.]*")) {
+						field.setText(newValue.replaceAll("[^0-9.]*", ""));
+					}
+				} else {
+					if (!newValue.matches("[0-9]*")) {
+						field.setText(newValue.replaceAll("[^0-9]*", ""));
+					}
+				}
+				try {
+					Double.valueOf(newValue);
+					field.setStyle("");
+				} catch (NumberFormatException ex) {
+					field.setStyle("-fx-control-inner-background:pink;");
+				}
+			}
+		});
+
+	}
+}
